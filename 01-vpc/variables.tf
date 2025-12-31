27913764aaeb5670556dab25358364a42b9087bb
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2025-12-31 17:01:58 +0100

    Step 1: Add variables for project_id and region

---
+++ 01-vpc/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+  default     = "us-central1"
+}
